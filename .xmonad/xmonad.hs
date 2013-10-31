1d39907b0fa665f73d9ca42103a7565c17a891f3
Michael Bastos <bastosmichael@gmail.com>
2013-10-31 12:20:31 -0700

    Cleaned up my xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,25 +3,28 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Actions.SpawnOn
 import System.IO
 
 myManageHook  = composeAll
 	[ className =? "emulator" --> doFloat
-	, className =? "google-chrome" --> doShift "1:web"
 	, manageDocks
 	]
 
+myWorkSpaces = [ "web" -- Workspace 1
+	       , "dev" -- Workspace 2
+	       ]
+
+myStartupHook :: X ()
+myStartupHook = do
+	spawnOn "web" "google-chrome"
+
 main = do
-    xmproc <- spawnPipe "/usr/bin/xmobar"
-    xmonad $ defaultConfig
-        { workspaces = ["1:web","2:dev","3","4","5","6","7","8","9","0","-","="]
-	, manageHook  = myManageHook <+> manageHook defaultConfig
-        , layoutHook  = avoidStruts $ layoutHook defaultConfig
-        , logHook     = dynamicLogWithPP xmobarPP
-            { ppOutput = hPutStrLn xmproc
-            , ppTitle = xmobarColor "green" "" . shorten 50
-            }
-        , borderWidth = 2
-        , terminal    = "urxvtc" } `additionalKeys`
-        [ ((mod4Mask    ,xK_l), spawn "slock") 
-        ]
+    xmonad =<< xmobar defaultConfig
+        { XMonad.workspaces  = myWorkSpaces 
+	, XMonad.manageHook  = myManageHook <+> manageHook defaultConfig
+        , XMonad.layoutHook  = avoidStruts $ layoutHook defaultConfig
+        , XMonad.borderWidth = 2
+        , XMonad.terminal    = "urxvtc"
+	, XMonad.startupHook = myStartupHook
+	}
