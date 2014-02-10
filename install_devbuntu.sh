0aed342d4ad3dc1e03afcff13a786b1b9faeb93f
Michael Bastos <bastosmichael@gmail.com>
2014-02-10 08:51:21 -0800

    Update install_devbuntu.sh

--- install_devbuntu.sh
+++ install_devbuntu.sh
@@ -2,7 +2,7 @@
 
 if [ $# -eq 0 ]
 then
-        echo "install_devbuntu.sh [options] install|update|upgrade|ssh"
+        echo "install_devbuntu.sh [options] install|update|upgrade|android|devops|ssh"
 elif [ $1 == 'install' ]; then
 	echo "Updating..."
 	sudo apt-get update
@@ -13,8 +13,6 @@ elif [ $1 == 'install' ]; then
 	sudo apt-get -qqy update
 	sudo apt-get -qqy install libncurses5:i386 libstdc++6:i386 zlib1g:i386
 	sudo apt-get install -y build-essential screen git-core curl openssh-client openssh-server libxslt-dev libxml2-dev nmap winusb pcscd coolkey meld nautilus-dropbox
-	echo "Installing Heroku Toolset"
-	wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 	echo "Installing Databases"
 	sudo debconf-set-selections <<< 'mysql-server-<version> mysql-server/root_password password password'
 	sudo debconf-set-selections <<< 'mysql-server-<version> mysql-server/root_password_again password password'
@@ -34,35 +32,13 @@ elif [ $1 == 'install' ]; then
 	bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
 	source ~/.bashrc
 	type rvm | head -1
+	rvm install 2.1.0
 	rvm install 2.0.0
 	rvm install 1.9.3
 	rvm install 1.9.2
 	rvm install 1.8.7
-	rvm use --default 2.0.0
+	rvm use --default 2.1.0
 	sudo apt-get install -y libmysql-ruby ruby1.9.1-dev libv8-dev graphviz
-	echo "Installing Android SDK"
-	sudo apt-get install -y openjdk-7-jre openjdk-7-jdk
-	wget http://dl.google.com/android/android-sdk_r22.3-linux.tgz
-	sudo tar -xvf android-sdk_r22.3-linux.tgz -C /opt
-	sudo chmod -R 777 /opt/android-sdk-linux
-	rm *.tgz
-	echo "Installing Eclipse"
-	sudo apt-get -y install eclipse eclipse-jdt eclipse-cdt eclipse-pde eclipse-platform eclipse-rcp
-	echo "Installing Docker"
-	sudo apt-get update
-	sudo apt-get -y install linux-image-extra-`uname -r`
-	sudo sh -c "wget -qO- https://get.docker.io/gpg | apt-key add -"
-	sudo sh -c "echo deb http://get.docker.io/ubuntu docker main\ > /etc/apt/sources.list.d/docker.list"
-	sudo apt-get update
-	sudo apt-get -y install lxc-docker
-	echo "Installing Vagrant"
-	sudo apt-get -y install virtualbox vagrant linux-headers-$(uname -r)
-	sudo dpkg-reconfigure virtualbox-dkms
-	vagrant plugin install vagrant-aws vagrant-awsinfo vagrant-rackspace
-	echo "Installing VM Ware"
-	wget https://www.dropbox.com/s/zdqk3rza7ipd70a/VMware-Workstation-Full-10.0.1-1379776.x86_64.bundle
-	chmod 777 VMware-Workstation-Full-10.0.1-1379776.x86_64.bundle
-	sudo ./VMware-Workstation-Full-10.0.1-1379776.x86_64.bundle
 elif [ $1 == 'update' ]; then
     echo "Updating..."
     sudo apt-get update
@@ -78,4 +54,31 @@ elif [ $1 == 'ssh' ]; then
 	ssh-keygen -t rsa -C "bastosmichael@gmail.com"
 	cd ~/
 	cat ~/.ssh/id_rsa.pub
+elif [ $1 == 'android' ]; then
+	echo "Installing Android SDK"
+	sudo apt-get install -y openjdk-7-jre openjdk-7-jdk
+	wget http://dl.google.com/android/android-sdk_r22.3-linux.tgz
+	sudo tar -xvf android-sdk_r22.3-linux.tgz -C /opt
+	sudo chmod -R 777 /opt/android-sdk-linux
+	rm *.tgz
+	echo "Installing Eclipse"
+	sudo apt-get -y install eclipse eclipse-jdt eclipse-cdt eclipse-pde eclipse-platform eclipse-rcp
+elif [ $1 == 'devops' ]; then
+	echo "Installing Docker"
+	sudo apt-get update
+	sudo apt-get -y install linux-image-extra-`uname -r`
+	sudo sh -c "wget -qO- https://get.docker.io/gpg | apt-key add -"
+	sudo sh -c "echo deb http://get.docker.io/ubuntu docker main\ > /etc/apt/sources.list.d/docker.list"
+	sudo apt-get update
+	sudo apt-get -y install lxc-docker
+	echo "Installing Vagrant"
+	sudo apt-get -y install virtualbox vagrant linux-headers-$(uname -r)
+	sudo dpkg-reconfigure virtualbox-dkms
+	vagrant plugin install vagrant-aws vagrant-awsinfo vagrant-rackspace
+	echo "Installing VM Ware"
+	wget https://www.dropbox.com/s/zdqk3rza7ipd70a/VMware-Workstation-Full-10.0.1-1379776.x86_64.bundle
+	chmod 777 VMware-Workstation-Full-10.0.1-1379776.x86_64.bundle
+	sudo ./VMware-Workstation-Full-10.0.1-1379776.x86_64.bundle
+	echo "Installing Heroku Toolset"
+	wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 fi
